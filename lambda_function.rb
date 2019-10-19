b692fcff7ec976f783b0e9ed340cabb5c6477359
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-10-18 23:45:08 -0500

    Update lambda_function.rb

--- lambda_function.rb
+++ lambda_function.rb
@@ -32,7 +32,7 @@ def lambda_handler(event:, context:)
         end
     
         unless previous_links.include?(title)
-            twitter.update(title.include?("web design") ? "#{title} #design #webdesign #{link}" : "#{title} #{link} #design")
+            twitter.update(title.include?("Web Design") ? "#{title} #design #webdesign #{link}" : "#{title} #{link} #design")
         end
     end
     { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
